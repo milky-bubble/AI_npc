317de70474ee92ae4eb1896cc91c565f96d5b48f
wangpuli <wangpuli@hotmail.com>
2019-05-20 23:11:17 +0800

    calculate the safe region

---
+++ AItest1/AI.h
@@ -0,0 +1,10 @@
+#ifndef AI_H_INCLUDED
+#define AI_H_INCLUDED
+
+#define WIDTH 20
+#define HEIGHT 15
+#define FLOOR 1
+#define BUBBLEWAVE
+#define delay
+
+#endif // AI_H_INCLUDED
--- AItest1/main.cpp
+++ AItest1/main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
-
+#include "AI.h"
 using namespace std;
 
+class AI{
+    private int block[WIDTH][HEIGHT];
+    private bool record[WIDTH][HEIGHT];
+    //calculate the safe region
+    void computeSafeRegion(){
+        int countTime[WIDTH][HEIGHT]={0};//calculate if the bomb is still there
+        //deal with the exploring bomb
+        for(int i=0;i<WIDTH;i++)
+        {
+            for(int j=0;j<HEIGHT;j++)
+            {
+                if(block[i][j]==FLOOR||block[i][j]==BUBBLEWAVE)//only floor can walk on
+                {
+                    if(countTime>=delay)
+                    {
+                        record[i][j]=true;
+                        countTime[i][j]=0;
+                    }
+                    else{
+                        record[i][j]=false;
+                        countTime[i][j]++;
+                    }
+                }
+                else{
+                    record[i][j]=false;
+                    countTime[i][j]=0;
+                }
+            }
+        }
+        //deal with the bomb in the future
+        for(int i=0;i<WIDTH;i++)
+        {
+            for(int j=0;j<HEIGHT;j++)
+            {
+
+            }
+        }
+
+    }
+
+};
 int main()
 {
     cout << "Hello world!" << endl;
